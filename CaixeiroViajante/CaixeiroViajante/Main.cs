73effef2d6bc5e2a317fd96f880bd6c6ec8edf34
alisson.sganzerla <alisson.sganzerla@gmailcom>
2021-09-05 01:33:28 -0300

    feat: start project

---
+++ CaixeiroViajante/CaixeiroViajante/Main.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace CaixeiroViajante
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            ProblemaTSP MeuProblema = new(numeroPontos:8);
+            MeuProblema.GerarPontosAleatorios();
+            MeuProblema.CalcularMatrizDistancias();
+        }
+    }
+}
---
+++ CaixeiroViajante/CaixeiroViajante/ProblemaTSP.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace CaixeiroViajante
+{
+    class ProblemaTSP
+    {
+        private int _numeroPontos;
+        public double[,] Coordenadas;
+        public double[,] MatrizDistancias;
+
+
+
+        public ProblemaTSP(int numeroPontos)
+        {
+            _numeroPontos = numeroPontos;
+        }
+
+
+        public   void GerarPontosAleatorios()
+        {
+            Coordenadas = new double[_numeroPontos, 2];
+            Random aleatorio = new(1);
+            for (int i =0; i< _numeroPontos; i++)
+            {
+                Coordenadas[i, 0] = 10 + aleatorio.NextDouble()*490;
+                Coordenadas[i, 1] = 10 + aleatorio.NextDouble()*490;
+            }
+        }
+
+        public   void CalcularMatrizDistancias()
+        {
+            MatrizDistancias = new double[_numeroPontos, _numeroPontos];
+            for(int i=0; i< _numeroPontos; i++)
+            {
+                for (int j=0; j < _numeroPontos; j++)
+                {
+                    double x1 = Coordenadas[i, 0];
+                    double y1 = Coordenadas[i, 1];
+                    double x2 = Coordenadas[j, 0];
+                    double y2 = Coordenadas[j, 1];
+                    MatrizDistancias[i, j] = CalcularDistanciaEuclidiana2Pontos(x1, y1, x2, y2);
+                 }
+            }
+            Console.WriteLine(MatrizDistancias);
+        }
+
+        public static double CalcularDistanciaEuclidiana2Pontos(double x1, double y1, double x2, double y2)
+        {
+            // fórmula distância euclidiana entre dois pontos
+            // raiz((x2-x1)^2 + (y2-y1)^2)
+            double deltaX = x2 - x1;
+            double deltaY = y2 - y1;
+            return Math.Sqrt(deltaX * deltaX + deltaY * deltaY);
+        }
+
+
+
+
+    }
+}
